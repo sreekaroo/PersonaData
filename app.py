1ab314c35976a6ec04b4de8abd53b75c5aa14d5a
sreekaroo <46632027+sreekaroo@users.noreply.github.com>
2022-01-05 14:17:50 -0500

    serving on port 80

--- app.py
+++ app.py
@@ -257,4 +257,4 @@ def results():
 
 if __name__ == "__main__":
     # app.run(host="0.0.0.0", port=8080, debug=True)
-    serve(app, host="0.0.0.0", port=8080)
+    serve(app, host="0.0.0.0", port=80)
